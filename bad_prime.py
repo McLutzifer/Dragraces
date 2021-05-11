292255700caa559b8245966ff361bc697fb427eb
McLutzifer <lukasschumi@gmx.at>
2021-05-11 19:55:11 +0200

    buggy

--- bad_prime.py
+++ bad_prime.py
@@ -1,3 +1,4 @@
+"""
 def is_prime(n):
     for i in range(2, n):
         if n % i == 0:
@@ -6,3 +7,21 @@ def is_prime(n):
             print(i)
 
 is_prime(20)
+"""
+
+
+# Program to check if a number is prime or not
+
+num = 29
+
+
+flag = False
+
+for i in range(2, num):
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+    if not flag:
+        print(num, "is  a prime number")
+
