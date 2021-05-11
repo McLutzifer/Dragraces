085b57ed71ef6f101a00da5d89d01dbced9c1f59
McLutzifer <lukasschumi@gmx.at>
2021-05-11 19:55:40 +0200

    fix typo

--- bad_prime.py
+++ bad_prime.py
@@ -17,11 +17,11 @@ num = 29
 
 flag = False
 
+
 for i in range(2, num):
-    for i in range(2, num):
-        if (num % i) == 0:
-            flag = True
-            break
-    if not flag:
-        print(num, "is  a prime number")
+    if (num % i) == 0:
+        flag = True
+        break
+if not flag:
+    print(num, "is  a prime number")
 
