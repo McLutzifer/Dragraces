1f5b0bf6181b4b7444d1623e3e70ca1e1c529c3f
McLutzifer <lukasschumi@gmx.at>
2021-05-11 19:57:23 +0200

    update

--- bad_prime.py
+++ bad_prime.py
@@ -1,27 +1,19 @@
-"""
-def is_prime(n):
-    for i in range(2, n):
-        if n % i == 0:
-            return false
-        else:
-            print(i)
 
-is_prime(20)
-"""
+def is_prime(num):
+    flag = False
+    for i in range(2, num):
+        if (num % i) == 0:
+            flag = True
+            break
+    if not flag:
+        print(num, "is  a prime number")
 
-
-# Program to check if a number is prime or not
-
-num = 29
+for n in range(2, 29):
+    is_prime(n)
 
 
-flag = False
 
 
-for i in range(2, num):
-    if (num % i) == 0:
-        flag = True
-        break
-if not flag:
-    print(num, "is  a prime number")
+
+
 
